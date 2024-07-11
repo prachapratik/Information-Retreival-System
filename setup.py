008a96e92a63c2d129637221b71f374cb212fe4b
Pratik Chakravorty <pratik.chakravorty1989@gmail.com>
2024-07-11 15:00:05 +0530

    REQUIREMENTS ADDED

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Pratik',
+    author_email= 'pracha.pratik@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
